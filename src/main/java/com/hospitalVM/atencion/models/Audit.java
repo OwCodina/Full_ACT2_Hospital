3c187f6021a47dea4e80945e1c75dddd6d0072d2
VA-Alumno_owen <ow.codina@duocuc.cl>
2026-04-09 11:18:23 -0400

    Base_models_ini

---
+++ src/main/java/com/hospitalVM/atencion/models/Audit.java
@@ -0,0 +1,19 @@
+package com.hospitalVM.atencion.models;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Embeddable
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Audit {
+
+
+}
